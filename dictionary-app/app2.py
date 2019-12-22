4961fdab39f082a4a1d173b620dfb13fa627530d
Tak1za <varungupta2015135@gmail.com>
2019-12-22 12:19:24 +0530

    Used database for dictionary

--- dictionary-app/app2.py
+++ dictionary-app/app2.py
@@ -1,29 +1,66 @@
-import json
-import os
+# import json
+# import os
 import difflib
 from difflib import get_close_matches
+import mysql.connector
 
-data = json.load(open("data.json"))
+con = mysql.connector.connect(
+    user = "ardit700_student",
+    password = "ardit700_student",
+    host = "108.167.140.122",
+    database = "ardit700_pm1database"
+)
 
-def definition(word):
-    word = word.lower()
-    if word in data:
-        return data[word]
-    elif word.title() in data:
-        return data[word.title()]
-    elif word.upper() in data:
-        return data[word.upper()]
-    elif len(get_close_matches(word, data.keys())) > 0:
-        closestMatch = get_close_matches(word, data.keys())[0]
-        response = input("Did you mean {} instead? Enter Y if yes, or N if no. : ".format(closestMatch))
-        if response.lower() == "y":
-            return data[closestMatch]
-        elif response.lower() == "n":
-            return "Word doesn't exist, please double check!"
-        else:
-            return "We didn't understand your response."
-    else:
-        return "Word doesn't exist, please double check!"
+cursor = con.cursor()
 
 word = input("Enter word: ")
-print("\n".join(definition(word)))
+
+query = cursor.execute("select * from Dictionary where Expression = '{}'".format(word))
+results = cursor.fetchall()
+
+query = cursor.execute("select Expression from Dictionary")
+resultsExpressions = cursor.fetchall()
+
+if results:
+    for result in results:
+        print (result[1])
+elif len(get_close_matches(word, [i for r in resultsExpressions for i in r])) > 0:
+    closestMatch = get_close_matches(word, [i for r in resultsExpressions for i in r])[0]
+    response = input("Did you mean {} instead? Enter Y if yes, or N if no. : ".format(closestMatch))
+    if response.lower() == "y":
+        query = cursor.execute("select Definition from Dictionary where Expression = '{}'".format(closestMatch))
+        results = cursor.fetchall()
+        for result in results:
+            print (result[0])
+    elif response.lower() == "n":
+        print ("No word found")
+    else:
+        print ("We didn't understand your response")
+else:
+    print ("No word found")
+
+# data = json.load(open("data.json"))
+
+# def definition(word):
+#     word = word.lower()
+#     if word in data:
+#         return data[word]
+#     elif word.title() in data:
+#         return data[word.title()]
+#     elif word.upper() in data:
+#         return data[word.upper()]
+#     elif len(get_close_matches(word, data.keys())) > 0:
+#         test = data.keys()
+#         closestMatch = get_close_matches(word, data.keys())[0]
+#         response = input("Did you mean {} instead? Enter Y if yes, or N if no. : ".format(closestMatch))
+#         if response.lower() == "y":
+#             return data[closestMatch]
+#         elif response.lower() == "n":
+#             return "Word doesn't exist, please double check!"
+#         else:
+#             return "We didn't understand your response."
+#     else:
+#         return "Word doesn't exist, please double check!"
+
+# word = input("Enter word: ")
+# print("\n".join(definition(word)))
