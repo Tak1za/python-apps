5e48e428e00d8fb45b33fac7cac80da0d0070d6e
Tak1za <varungupta2015135@gmail.com>
2019-12-22 12:31:36 +0530

    added config file for database connection params

--- dictionary-app/app2.py
+++ dictionary-app/app2.py
@@ -3,12 +3,13 @@
 import difflib
 from difflib import get_close_matches
 import mysql.connector
+import config
 
 con = mysql.connector.connect(
-    user = "ardit700_student",
-    password = "ardit700_student",
-    host = "108.167.140.122",
-    database = "ardit700_pm1database"
+    user = config.user,
+    password = config.password,
+    host = config.host,
+    database = config.database
 )
 
 cursor = con.cursor()
